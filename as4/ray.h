511acff6daacf985a821042f7da377651f1d3f15
Sameep Tandon <tandon@eecs.berkeley.edu>
2011-09-27 22:00:18 -0700

    fuck git

--- as4/ray.h
+++ as4/ray.h
@@ -6,8 +6,9 @@ USING_PART_OF_NAMESPACE_EIGEN
 class ray {
   public:
     ray(Vector2d, Vector3d, Vector3d, int);
+    ~ray();
   private:
     Vector2d _pixel;
     Vector3d _origin, _direction;
     int _depth;
-}
+};
