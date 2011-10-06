84c08570a4cefe287040738ed2b89849045d5d8d
Daiwei Li <daiweili@gmail.com>
2011-10-06 15:58:32 -0700

    Actually adding Ellipsoids

---
+++ as4/Ellipsoid.cpp
@@ -0,0 +1,119 @@
+#include "Ellipsoid.h"
+
+void rotation_from_euler_angles(Matrix3d& rotation, double angZ, double angY, double angX)
+{
+  Matrix3d rot1(Eigen::AngleAxisd(angZ, Vector3d::UnitZ()));
+  Vector3d axis2 = rot1*(Vector3d::UnitY());
+  Matrix3d rot2 = Eigen::AngleAxisd(angY, axis2)*rot1;
+  Vector3d axis3 = rot2*(Vector3d::UnitX());
+  rotation = Eigen::AngleAxisd(angX, axis3)*rot2;
+}
+
+void euler_angles_from_rotation(const Matrix3d& rotation, double& angZ, double& angY, double& angX)
+{
+  angZ = atan2(rotation(1,0), rotation(0,0));
+  angY = atan2(-rotation(2,0), sqrt(rotation(2,1)*rotation(2,1) + rotation(2,2)*rotation(2,2)));
+  angX = atan2(rotation(2,1), rotation(2,2));
+}
+
+Ellipsoid::Ellipsoid(Vector3d ka, Vector3d kd, Vector3d ks, Vector3d km, double sp, Vector3d scale, Vector3d translation, Vector3d rotation) :
+  _scale(scale),
+  _translation(translation),
+  _rotation(rotation)
+{
+  _center = Vector3d(0.0, 0.0, 0.0);
+  _radius = 1.0;
+  _ka = ka;
+  _kd = kd;
+  _ks = ks;
+  _km = km;
+  _sp = sp;
+  // Calculate _M and _M_inverse
+  Matrix3d rot; 
+  rotation_from_euler_angles(rot, rotation(0), rotation(1), rotation(2));
+  _M << scale(0), 0, 0,
+     0, scale(1), 0,
+     0, 0, scale(2);
+  _M = rot * _M;
+  _M_inverse = _M.inverse();
+};
+
+/**
+ * This returns the vector from the center to the surface
+ */
+Vector3d Ellipsoid::normal(Vector3d point) {
+  //std::cout << (point-_center).norm() - _radius << std::endl;
+  assert((point-_translation).norm() - _scale.norm() < 0.001);
+  return point-_translation;
+}
+
+bool Ellipsoid::intersect(Ray& r, double &t) {
+  Vector3d ray_dir, ray_orig;
+  Vector3d ray_dir_unit_sphere, ray_orig_unit_sphere;
+  r.getDirection(ray_dir);
+  r.getOrigin(ray_orig);
+  ray_dir_unit_sphere = _M_inverse * ray_dir;
+  ray_orig_unit_sphere = _M_inverse * (ray_orig - _translation);
+  //Ray r_unit_sphere = Ray(Vector2d(0,0), ray_dir_unit_sphere, ray_orig_unit_sphere, 0);
+  /*
+   * cout << ray_dir.transpose() << endl;
+  cout << ray_dir_unit_sphere.transpose() << endl;
+  cout << ray_orig.transpose() << endl;
+  cout << ray_orig_unit_sphere.transpose() << endl;
+  exit(0);
+*/
+  //Compute A, B and C coefficients
+  double a = ray_dir_unit_sphere.dot(ray_dir_unit_sphere);
+  double b = 2 * ray_dir_unit_sphere.dot(ray_orig_unit_sphere-_center);
+  double c = (ray_orig_unit_sphere-_center).dot(ray_orig_unit_sphere-_center) - (_radius * _radius);
+
+  //Find discriminant
+  double disc = b * b - 4 * a * c;
+
+  // if discriminant is negative there are no real roots, so return 
+  // false as ray misses sphere
+  if (disc < 0)
+    return false;
+
+  // compute q as described above
+  double distSqrt = sqrt(disc);
+  double q;
+  if (b < 0)
+    q = (-b - distSqrt)/2.0;
+  else
+    q = (-b + distSqrt)/2.0;
+
+  // compute t0 and t1
+  double t0 = q / a;
+  double t1 = c / q;
+
+  // make sure t0 is smaller than t1
+  if (t0 > t1)
+  {
+    // if t0 is bigger than t1 swap them around
+    double temp = t0;
+    t0 = t1;
+    t1 = temp;
+  }
+
+  // if t1 is less than zero, the object is in the ray's negative direction
+  // and consequently the ray misses the sphere
+  if (t1 < 0)
+    return false;
+
+  // if t0 is less than zero, the intersection point is at t1
+  if (t0 < 0)
+  {
+    t = t1;
+    return true;
+  }
+  // else the intersection point is at t0
+  else
+  {
+    t = t0;
+    return true;
+  }
+ 
+}
+
+
---
+++ as4/Ellipsoid.h
@@ -0,0 +1,21 @@
+#ifndef _ELLIPSOID_H
+#define _ELLIPSOID_H
+#include "Sphere.h"
+
+class Ellipsoid : public Sphere {
+  protected:
+    // Scale: x,y,z radii
+    // Translate: x,y,z from origin
+    // Rotation: about x,y,z axes
+    Vector3d _scale, _translation, _rotation;
+    // M is R * S
+    Matrix3d _M;
+    Matrix3d _M_inverse;
+    Sphere _sphere;
+  public:
+    Ellipsoid(Vector3d, Vector3d, Vector3d, Vector3d, double, Vector3d, Vector3d, Vector3d);
+    ~Ellipsoid();
+    Vector3d normal(Vector3d);
+    bool intersect(Ray&, double&);
+};
+#endif //_ELLIPSOID_H
